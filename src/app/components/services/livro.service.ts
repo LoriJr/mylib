a67e043fff007c346946b60dd81316d7305d4866
lorijr <jrsilva.lorivas@gmail.com>
2025-12-16 17:39:30 -0300

    adicionado estilo aos cards

--- src/app/components/services/livro.service.ts
+++ src/app/components/services/livro.service.ts
@@ -1,8 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LivroService {
+
+  constructor(private http: HttpClient){
+    
+
+  }
 
 }
